c81a3c1b52e72de81cadd168c1bc04e3a6337fbb
Ruslan Kornev <oganer@gmail.com>
2010-05-03 14:28:19 +0400

    .

--- vendor/plugins/formtastic_calendar_date_select/rails/init.rb
+++
@@ -1,2 +0,0 @@
-require File.join(File.dirname(__FILE__), *%w[.. lib formtastic_calendar_date_select])
-Formtastic::SemanticFormBuilder.send(:include, DavidOshiro::Formtastic::CalendarDateSelect)
--- vendor/plugins/rails-ckeditor/uninstall.rb
+++
@@ -1,8 +0,0 @@
-directory = File.join(RAILS_ROOT, '/vendor/plugins/rails-ckeditor/')
-
-require "#{directory}lib/ckeditor/utils"
-require "#{directory}lib/ckeditor/version"
-
-puts "** Uninstalling CKEditor Plugin version #{Ckeditor::Version.current}...."
-
-Ckeditor::Utils.destroy
