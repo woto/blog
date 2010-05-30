11815ca2dd8599b77fec3a940d8bf7d879e03825
Ruslan Kornev <oganer@gmail.com>
2010-05-31 01:50:37 +0400

    Сделано сделал менюшки для связного брождения по сайту, улучшен дизайн

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 
 module ApplicationHelper
+  def post_category(post)
+    out = ""
+    if post.cached_category
+      out << "в категории "
+      out << post.cached_category.map{|item| link_to(item.name, link_category(item.name, false), :rel => link_category(item.name, true))}.join(' → ')
+    #else
+    #  out << "без категории "
+    end
+  end
 
   def categories
     out = ""
@@ -12,15 +21,15 @@ module ApplicationHelper
 
         if !cat[i+1].nil? && cat[i+1].lvl.to_i > c.lvl.to_i
           counter += 1
-          out << "<li>" + link_to(c.name, search_posts_path(:date => params[:date], :tags => params[:tags], :category => c.name)) + '<ul class="links">'
+          out << "<li>" + link_to(c.name, link_category(c.name, false), :rel => link_category(c.name, true)) + '<ul class="links">'
         end
 
         if !cat[i+1].nil? && cat[i+1].lvl.to_i == c.lvl.to_i
-          out << "<li>" + link_to(c.name, search_posts_path(:date => params[:date], :tags => params[:tags], :category => c.name)) + "</li>"
+          out << "<li>" + link_to(c.name, link_category(c.name, false), :rel => link_category(c.name, true))  + "</li>"
         end
 
         if !cat[i+1].nil? && cat[i+1].lvl.to_i < c.lvl.to_i
-          out << "<li>" + link_to(c.name, search_posts_path(:date => params[:date], :tags => params[:tags], :category => c.name)) + "</li>"
+          out << "<li>" + link_to(c.name, link_category(c.name, false), :rel => link_category(c.name, true)) + "</li>"
           counter -= 1
           (c.lvl.to_i - cat[i+1].lvl.to_i).times do
               out << "</ul></li>"
@@ -28,7 +37,7 @@ module ApplicationHelper
         end
         
         if (cat[i+1].nil?)
-          out << "<li>" + link_to(c.name, search_posts_path(:date => params[:date], :tags => params[:tags], :category => c.name)) + "</li>"
+          out << "<li>" + link_to(c.name, link_category(c.name, false), :rel => link_category(c.name, true)) + "</li>"
           counter.times do
             out << "</ul></li>"
           end
@@ -41,31 +50,35 @@ module ApplicationHelper
   def filters
     if params[:category] || params[:tags] || params[:date]
       out = ""
-      out << "<div class='box'>"
+      out << "<div class='box' style='position: relative'>"
+      out << "<div style='position: absolute; top: 0px; right: 6px'>" + link_to("x", posts_path) + "</div>"
       if params[:category] and category = Category.find_by_name(params[:category])
           out << "<div>"
-          out << "Просмотр происходит внутри категории: ( "
+          out << link_to("-", search_posts_path(:date => params[:date], :tags => params[:tags]))
+          out << " Просмотр происходит внутри категории: "
           category.self_and_ancestors.each do |item|
-            out << link_to(item.name, search_posts_path(:category => item.name))
+            out << link_to(item.name, link_category(item.name, false), :rel => link_category(item.name, true))
             out << " → " unless category.self_and_ancestors.last == item 
           end 
-          out << " ) "
-          out << link_to("<sup>x</sup>", search_posts_path(:date => params[:date], :tags => params[:tags]))
           out << "</div>"
       end
       if params[:tags]
         out << "<div>"
         tags = Array.[](params[:tags]).flatten
-        out << "Включен фильтр по" +  Russian.p(tags.count, "тегу", "тегам", "тегам") + ":"
+        out << link_to("-", search_posts_path(:date => params[:date], :category => params[:category]))
+        out << " Включен фильтр по " +  Russian.p(tags.count, "тегу", "тегам", "тегам") + ": "
         tags.each { |tag|
-          out << " ( " + link_to(h(tag), search_posts_path(:tags => tag)) + " ) "
-          out << link_to("<sup>x</sup>", search_posts_path(:date => params[:date], :tags => tags.reject{|item| item == tag}, :category => params[:category]))
+          out << link_to("-", search_posts_path(:date => params[:date], :tags => tags.reject{|item| item == tag}, :category => params[:category]))
+          out << "&nbsp;"
+          out << link_to(h(tag), search_posts_path(:tags => tag))
+          out << ", " unless tag == tags.last
         }
         out << "</div>"
       end
       if params[:date]
         out << "<div>"
-        out << "Включен фильтр по дате: ( "
+        out << link_to("-", search_posts_path(:tags => params[:tags], :category => params[:category]))
+        out << " Включен фильтр по дате: "
         out << "<a href='"
         out << url_for(search_posts_path(:date => params[:date]))
         out << "'>"
@@ -75,15 +88,33 @@ module ApplicationHelper
           out << Russian.strftime(@date, "%d %B %Y")
         end
         out << "</a>"
-        out << " ) "
-        out << link_to("<sup>x</sup>", search_posts_path(:tags => params[:tags], :category => params[:category]))
         out << "</div>"
       end
       out << "</div>"
     end
   end
 
-  def link_tags(tags)
-    search_posts_path(:tags => tags, :category => params[:category], :date => params[:date])
+  def link_tags(tags, save_state)
+    if save_state
+      search_posts_path(:tags => tags, :category => params[:category], :date => params[:date])
+    else
+      search_posts_path(:tags => tags)
+    end
+  end
+
+  def link_category(category, save_state)
+    if save_state
+      search_posts_path(:tags => params[:tags], :category => category, :date => params[:date])
+    else
+      search_posts_path(:category => category)
+    end
+  end
+
+  def link_calendar(date, save_state)
+    if save_state
+      search_posts_path(:tags => params[:tags], :category => params[:category], :date => date)
+    else
+      search_posts_path(:date => date)
+    end
   end
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,30 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+    $('a').each(function(i, e){
+      $(e).mouseenter(function(){
+       if($(this).attr('rel') && $(this).attr('rel').match(/\/.*/i)) {
+        toolbox = $("<div class='box'></div>");
+        toolbox.css("position", "absolute");
+        toolbox.css("top", $(this).position().top - 13);
+        toolbox.css("background", "#EEEEEE");
+        toolbox.css("padding", "4px");
+        toolbox.html( 
+          "<a href='" + $(this).attr('href') + "'>" + "Без фильтра" + "</a>" +
+          "<br />" +
+          "<a href='" + $(this).attr('rel') + "'>" + "С фильтром" + "</a>");
+        $(this).append(toolbox);
+
+        if($(this).position().left + toolbox.width() > $('#wrapper').width())
+          toolbox.css("left", $("#wrapper").width() - toolbox.width());
+        else
+          toolbox.css("left", $(this).position().left);
+
+        toolbox.mouseleave(function(){
+          $(this).remove();
+        });
+        $(this).mouseleave(function(){
+          toolbox.remove();
+        });
+      }
+    });
+  });
+});
